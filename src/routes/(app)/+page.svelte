442ed76a671f1f92e979c3701ad94afde434d375
Ethan <ethan.steip@gmail.com>
2024-08-19 22:02:00 -0400

    adding button to test api call to express app

--- src/routes/(app)/+page.svelte
+++ src/routes/(app)/+page.svelte
@@ -1,6 +1,13 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button/index';
 	import { toast } from 'svelte-sonner';
+	import { env } from '$env/dynamic/public';
+
+	const testFetch = async () => {
+		const response = await fetch(env.PUBLIC_SVELTE_BUILDER_API_URL + '/hello');
+		const data = await response.json();
+		console.log('Data: ', data);
+	};
 </script>
 
 <main class=" flex h-screen w-full flex-col items-center justify-center">
@@ -8,5 +15,6 @@
 	<div class="mt-3 flex w-full flex-col items-center justify-center gap-y-2">
 		<h2 class="text-xl">Web development starter kits, built your way.</h2>
 		<Button href="/builder/whats-your-style">Get Started</Button>
+		<Button on:click={testFetch}>Click Me</Button>
 	</div>
 </main>
