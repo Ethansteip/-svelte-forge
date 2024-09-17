65ae4e246bb53825361327a93ce3b278ab350c02
Ethan <ethan.steip@gmail.com>
2024-09-17 19:26:33 -0400

    continued work on shad preview

--- src/lib/components/builder/styles/ShadPreview.svelte
+++ src/lib/components/builder/styles/ShadPreview.svelte
@@ -4,10 +4,28 @@
 	import { Badge } from '$lib/components/ui/badge/index.js';
 	import * as Avatar from '$lib/components/ui/avatar/index.js';
 	import Input from '$lib/components/ui/input/input.svelte';
-	import { Cat, UserRoundPlus, Settings, EllipsisVertical, Search } from 'lucide-svelte';
+	import { DateFormatter, type DateValue, getLocalTimeZone } from '@internationalized/date';
+	import { cn } from '$lib/utils.js';
+	import { Calendar } from '$lib/components/ui/calendar/index.js';
+	import * as Popover from '$lib/components/ui/popover/index.js';
+	import {
+		Cat,
+		UserRoundPlus,
+		Settings,
+		EllipsisVertical,
+		Search,
+		Calendar as CalendarIcon
+	} from 'lucide-svelte';
+
+	const df = new DateFormatter('en-US', {
+		dateStyle: 'long'
+	});
+
+	let value: DateValue | undefined = undefined;
 </script>
 
-<section class="flex h-[65%] w-[80%] flex-col items-center justify-center gap-2">
+<!-- Navigation Bar -->
+<section class="flex h-auto w-[80%] flex-col items-center justify-center gap-2">
 	<nav class="flex w-full flex-row items-center justify-between bg-base-100">
 		<div class="flex items-center space-x-4 lg:space-x-6">
 			<div class="flex items-center space-x-3">
@@ -16,9 +34,11 @@
 				</Button>
 				<h3 class="tracking-wide">Svelte-Forge</h3>
 			</div>
-			<Button variant="link">Analytics</Button>
-			<Button variant="link">Dashboard</Button>
-			<Button variant="link">Sales</Button>
+			<div class="flex items-center space-x-1">
+				<Button variant="link">Analytics</Button>
+				<Button variant="link">Dashboard</Button>
+				<Button variant="link">Sales</Button>
+			</div>
 		</div>
 		<div class="flex items-center space-x-2">
 			<Button variant="outline" class="flex items-center gap-x-2">
@@ -30,12 +50,20 @@
 			</Button>
 		</div>
 	</nav>
-	<section class="mt-10 flex w-full items-center justify-between">
-		<Card.Root class="w-[30%]">
+
+	<!-- Customer Heading -->
+	<section class="mb-2 mt-4 flex w-full flex-col">
+		<h1 class="text-3xl font-bold tracking-wide">Customers</h1>
+		<h2 class="italic text-base-300">This is a component preview using ShadCn Svelte</h2>
+	</section>
+
+	<!-- Metrics & Date-picker -->
+	<section class="flex w-full items-center justify-between space-x-3">
+		<Card.Root class="w-1/3">
 			<Card.Header>
 				<div class="flex w-full items-center justify-between">
 					<div>
-						<Card.Title>Total Customers</Card.Title>
+						<Card.Title>Customers</Card.Title>
 						<Card.Description>year to date</Card.Description>
 					</div>
 					<Button variant="ghost" size="icon">
@@ -50,7 +78,7 @@
 				</div>
 			</Card.Content>
 		</Card.Root>
-		<Card.Root class="w-[30%]">
+		<Card.Root class="w-1/3">
 			<Card.Header>
 				<div class="flex w-full items-center justify-between">
 					<div>
@@ -69,7 +97,7 @@
 				</div>
 			</Card.Content>
 		</Card.Root>
-		<Card.Root class="w-[30%]">
+		<Card.Root class="w-1/3">
 			<Card.Header>
 				<div class="flex w-full items-center justify-between">
 					<div>
@@ -91,8 +119,28 @@
 			</Card.Content>
 		</Card.Root>
 	</section>
-	<section class="mt-3 flex w-full items-center justify-between">
-		<div class="flex w-[30%] items-center gap-x-3">
+	<section class="mt-3 flex w-full items-center justify-between space-x-3">
+		<div class="flex w-1/3 items-center">
+			<Popover.Root>
+				<Popover.Trigger asChild let:builder>
+					<Button
+						variant="outline"
+						class={cn(
+							'w-[280px] justify-start text-left font-normal',
+							!value && 'text-muted-foreground'
+						)}
+						builders={[builder]}
+					>
+						<CalendarIcon class="mr-2 h-4 w-4" />
+						{value ? df.format(value.toDate(getLocalTimeZone())) : 'Pick a date'}
+					</Button>
+				</Popover.Trigger>
+				<Popover.Content class="w-auto p-0">
+					<Calendar bind:value initialFocus />
+				</Popover.Content>
+			</Popover.Root>
+		</div>
+		<div class="flex w-2/3 items-center justify-end gap-x-3 2xl:w-[30%]">
 			<Input type="email" placeholder="email" class="max-w-xs" />
 			<Button>
 				<Search class="mr-2 h-4 w-4" />
